55f455cb78f1ce4d618fca0711a123a5fd4201b8
dbiesecke <webmaster@btc-mining.at>
2015-05-12 23:32:38 +0000

    added gist overview

--- tools/my-gist-info.pl
+++ tools/my-gist-info.pl
@@ -4,6 +4,9 @@ use strict;
 use warnings;
 use Pithub::Repos;
 use Pithub::Gists;
+use LWP::Simple;
+use Data::Dumper;
+
 
 my $r = Pithub::Repos->new(
     per_page        => 100,
@@ -22,11 +25,19 @@ while ( my $row = $result->next ) {
 
 print "---------------------------- GIST's ----------------\n\n";
 print "[Notes]()\n\n";
-
+    my $special;
    my $g = Pithub::Gists->new;
     $result = $g->list( user => 'dbiesecke' );
     if ( $result->success ) {
         while ( my $row = $result->next ) {
-            printf "   * [%s](%s)\n", $row->{description},$row->{html_url} || 'no description';
+            #print Dumper($row)."\n";
+#            printf "   * [%s](%s)\n", $row->{description},$row->{html_url} || 'no description';
+            my $files = $row->{files};
+             foreach my $file ( keys $files )  {
+                   next if not ($file =~/README.md/);
+#                   print Dumper($file)."\n";    
+            printf "   * [%s](%s)\n", $row->{description},$row->{html_url}."/raw/README.md" || 'no description';
+
+             }
         }
     }
