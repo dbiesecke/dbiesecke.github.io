ade35434fcdcd4ff93831a0d4e997dbf6bdb5ba1
dbiesecke <dbiesecke@gmail.com>
2019-12-16 20:50:59 +0100

    update kodi repo

--- repo/plugin.video.PsycoTV/Player.py
+++
@@ -1,7 +0,0 @@
- # -*- coding: utf-8 -*- 
-
-import base64, codecs
-import xbmcaddon
-
-MainBase = base64.b64decode('aHR0cHM6Ly9yYXcuZ2l0aHVidXNlcmNvbnRlbnQuY29tL1BzeWNvVFYvUHN5Y29UVi9tYXN0ZXIvcHN5Y28=')
-addon = xbmcaddon.Addon('plugin.video.PsycoTV')
