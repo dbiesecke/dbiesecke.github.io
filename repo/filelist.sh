0417bc6f03273356bc4e6085b639378178b0d7e6
dbiesecke <dbiesecke@gmail.com>
2020-01-12 11:03:07 +0100

    kodi

---
+++ repo/filelist.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+echo '# Stats'
+echo '========'
+echo ''
+echo ''
+echo 'IPTV Streams' $(cat plugin.video.iptvsimple.addons/resources/streams.m3u8| grep -v '#' | wc -l)
+echo 'Video Addons (Repo):' $(find . -iname "*.zip" | grep "plugin.video" | wc -l)
+echo ''
+echo ''
+echo '# Packages'
+echo '========'
+echo ''
+echo ''
+echo '| Packages                      | Version                 | URL                                                                                            |' 
+echo '|-------------------------------|-------------------------|------------------------------------------------------------------------------------------------|' 
+find . -iname "*.zip" -exec basename "{}" \; | sort | uniq | awk -F'-' '{print "| !["$1"](https://dbiesecke.github.io/repo/"$1"/icon.png) "$1"\t\t| "$2" | ["$1"](https://dbiesecke.github.io/repo/"$1"/"$1"-"$2") |"}' 
+
