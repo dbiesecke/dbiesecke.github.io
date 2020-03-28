ce125e2bb5b1504429c820fafe2319005302258e
dbiesecke <dbiesecke@gmail.com>
2020-03-28 11:48:04 +0100

    new addons

---
+++ repo/plugin.video.PsycoTVPro/resources/modules/user.py
@@ -0,0 +1,9 @@
+import base64 as b
+
+id   = b.b64decode('cGx1Z2luLnZpZGVvLlBzeWNvVFZQcm8=')
+
+name = b.b64decode('UHN5Y29UVg==')
+
+#host = b.b64decode('aHR0cDovLzE0NC4yMTcuNzguNzg=')
+host = b.b64decode('aHR0cDovL2xpdmUuZGFya3NpZGUtaXB0di5jb20=')
+port = b.b64decode('ODA4MA==')
